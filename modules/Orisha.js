804835ded04bd254d649397f7f4c48f80ffcb2ce
melsowole <82158719+melsowole@users.noreply.github.com>
2024-02-14 13:12:18 +0100

    updated main to display Obatala

--- modules/Orisha.js
+++ modules/Orisha.js
@@ -1,10 +1,11 @@
 import Interface from "./Interface.js";
 import Yemoja from "./characters/Yemoja.js";
 import Shango from "./characters/Shango.js";
+import Obatala from "./characters/Obatala.js";
 
 export class Orisha extends Interface {
   constructor() {
-    const characters = ["Yemoja", "Shango"];
+    const characters = ["Yemoja", "Shango", "Obatala"];
     super(characters);
   }
 }
@@ -16,6 +17,8 @@ export class CharacterSelection {
         return new Yemoja(playerName);
       case "Shango":
         return new Shango(playerName);
+      case "Obatala":
+        return new Obatala(playerName);
       default:
         break;
     }
